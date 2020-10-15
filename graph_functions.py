5e8b19f6b65ed37bb22dc8b3cb64aa0a1bf0e11a
decasppereira <43865518+decasppereira@users.noreply.github.com>
2020-10-15 23:42:57 +0100

    Update graph_functions.py

--- graph_functions.py
+++ graph_functions.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+import plfit
 
 def get_average_degree(G):
     return (nx.number_of_edges(G)*2)/nx.number_of_nodes(G)
@@ -12,18 +13,24 @@ def show_abolute_frequency(G):
     plt.scatter(x_axis, degree_list)
     plt.show()
 
+def power_law(x, a, b, c):
+    return (np.exp(-b * x) * a) + c
+
 def show_distribution(G):
     degree_list = nx.degree_histogram(G)
     max_degree = len(degree_list)
     N = nx.number_of_nodes(G)
     x_axis = [i for i in range(max_degree)]
     y_axis = [j/N for j in degree_list]
+    
     plt.xscale('log')
     plt.yscale('log')
     plt.xlim(0.01,max_degree)
-    plt.ylim(0.0001,0.5)
-    plt.scatter(x_axis, y_axis)
+    plt.ylim(0.0001,0.05)
+    
+    plt.scatter(x_axis, y_axis, s=1.5)
     plt.show()
+    
 
 def get_local_clustering(G, n):
     node_neighbors = list(nx.neighbors(G, n))
@@ -47,12 +54,28 @@ def get_average_clustering(G):
         sum += get_local_clustering(G,n)
     return sum/N
     
-def get_average_path_lenght(G):
+def get_average_path_length(G):
     return 0
 
+def show_hist_plot(G):
+    degree_list = nx.degree_histogram(G)
+    ax = sns.distplot(degree_list)
+    # plt.xscale('log')
+    # plt.yscale('log')
+    plt.show()
+
+def show_log_scale_dist(G):
+    degree_list = nx.degree_histogram(G)
+    max_degree = len(degree_list)
+    N = nx.number_of_nodes(G)
+    x = [i for i in range(max_degree)]
+    y = [j/N for j in degree_list]
+    z = np.polyfit(x, y, 3)
+    plt.show()
 
 if __name__ == '__main__':
     import graph_parser as gparser
     G = gparser.char_colab_graph()
-    print(get_local_clustering(G, 154))
+    show_log_scale_dist(G)
+    
     
