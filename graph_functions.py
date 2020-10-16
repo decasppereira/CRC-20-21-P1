3dc0c26d372f47f03b111d14019ee779e7dd9299
decasppereira <43865518+decasppereira@users.noreply.github.com>
2020-10-16 18:19:30 +0100

    Implemented show_power_law, get_avg_clustering and get_largest_component

--- graph_functions.py
+++ graph_functions.py
@@ -1,7 +1,8 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-import plfit
+from scipy.optimize import curve_fit
+
 
 def get_average_degree(G):
     return (nx.number_of_edges(G)*2)/nx.number_of_nodes(G)
@@ -13,24 +14,48 @@ def show_abolute_frequency(G):
     plt.scatter(x_axis, degree_list)
     plt.show()
 
-def power_law(x, a, b, c):
-    return (np.exp(-b * x) * a) + c
+def power_law(x, C, a):
+    return C * x ** (-a)
 
-def show_distribution(G):
+def linear_funct(x, lN, a):
+    return a * x + lN
+
+def show_linear_scale_dist(G):
     degree_list = nx.degree_histogram(G)
     max_degree = len(degree_list)
     N = nx.number_of_nodes(G)
     x_axis = [i for i in range(max_degree)]
     y_axis = [j/N for j in degree_list]
+        
+    plt.scatter(x_axis, y_axis, s=1.5)
+    plt.show()
+
+def show_log_scale_dist(G):
+    degree_list = nx.degree_histogram(G)
+    max_degree = len(degree_list)
+    N = nx.number_of_nodes(G)
+    x = [np.log10(i) for i in range(max_degree)]
+    y = [np.log10(j/N) for j in degree_list]
+    plt.scatter(x,y, s=1)
+    plt.show()
+
+def show_power_law(G):
+    degree_list = nx.degree_histogram(G)
+    max_degree = len(degree_list)
+    N = nx.number_of_nodes(G)
+
+    x = [i for i in range(max_degree)]
+    y = [j/N for j in degree_list]
+    
+    popt, pcov = curve_fit(power_law, x[2:], y[2:])
+
+    plt.scatter(x, y)
+    plt.plot(x, power_law(x, *popt), 'r')
     
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlim(0.01,max_degree)
-    plt.ylim(0.0001,0.05)
-    
-    plt.scatter(x_axis, y_axis, s=1.5)
     plt.show()
-    
+    print('Power law: Pk= '+ str(popt[0])+' * k** -'+ str(popt[1]))  
 
 def get_local_clustering(G, n):
     node_neighbors = list(nx.neighbors(G, n))
@@ -53,29 +78,21 @@ def get_average_clustering(G):
         print(n)
         sum += get_local_clustering(G,n)
     return sum/N
-    
-def get_average_path_length(G):
+
+def get_largest_component(G):
+    largest_cc =  max(nx.connected_components(G), key=len)
+    print("Size of the largest connected component: " + str(len(largest_cc)))
+    return G.subgraph(largest_cc).copy()
+
+
+#TODO 
+def get_betweeness_centrality(G):
     return 0
-
-def show_hist_plot(G):
-    degree_list = nx.degree_histogram(G)
-    ax = sns.distplot(degree_list)
-    # plt.xscale('log')
-    # plt.yscale('log')
-    plt.show()
-
-def show_log_scale_dist(G):
-    degree_list = nx.degree_histogram(G)
-    max_degree = len(degree_list)
-    N = nx.number_of_nodes(G)
-    x = [i for i in range(max_degree)]
-    y = [j/N for j in degree_list]
-    z = np.polyfit(x, y, 3)
-    plt.show()
 
 if __name__ == '__main__':
     import graph_parser as gparser
     G = gparser.char_colab_graph()
-    show_log_scale_dist(G)
+    #print("Average Degree: " + get_average_degree(G))
+    print("Average Path Length: " + str(nx.average_shortest_path_length(get_largest_component(G))))
     
     
